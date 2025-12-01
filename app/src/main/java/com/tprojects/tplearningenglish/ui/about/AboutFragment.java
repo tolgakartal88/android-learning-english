d0dfab1c5327e65c43d103d0a1fac693701e8719
tolgakartal <tolgakartal@hotmail.com.tr>
2025-12-01 23:18:24 +0300

    Create Menus, Fragments, Layouts

---
+++ app/src/main/java/com/tprojects/tplearningenglish/ui/about/AboutFragment.java
@@ -0,0 +1,33 @@
+package com.tprojects.tplearningenglish.ui.about;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.tprojects.tplearningenglish.databinding.FragmentAboutBinding;
+
+public class AboutFragment extends Fragment {
+    private FragmentAboutBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        AboutViewModel aboutViewModel =
+                new ViewModelProvider(this).get(AboutViewModel.class);
+
+        binding = FragmentAboutBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/com/tprojects/tplearningenglish/ui/words/WordsViewModel.java
@@ -0,0 +1,10 @@
+package com.tprojects.tplearningenglish.ui.words;
+
+import androidx.lifecycle.ViewModel;
+
+public class WordsViewModel extends ViewModel {
+    public WordsViewModel()
+    {
+
+    }
+}

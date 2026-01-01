7d9e8bd7fdd27d83457b2829b05b38439e1544aa
tolgakartal <tolgakartal@hotmail.com.tr>
2026-01-01 21:33:56 +0300

    WordsFragment English - Turkish Voice

--- app/src/main/java/com/tprojects/tplearningenglish/ui/words/WordsFragment.java
+++ app/src/main/java/com/tprojects/tplearningenglish/ui/words/WordsFragment.java
@@ -1,7 +1,10 @@
 package com.tprojects.tplearningenglish.ui.words;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +15,8 @@ import android.widget.ImageButton;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -25,10 +30,32 @@ import com.tprojects.tplearningenglish.R;
 import com.tprojects.tplearningenglish.data.dto.Words;
 import com.tprojects.tplearningenglish.data.repositories.WordsRepository;
 
+import java.util.ArrayList;
+
 public class WordsFragment extends Fragment {
 
     private WordsViewModel viewModel;
     private WordAdapter adapter;
+    private static final int REQUEST_CODE_SPEECH_INPUT = 1000;
+    private ActivityResultLauncher<Intent> speechResultLauncher;
+    private SearchView searchView;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Sonucu yakalamak için Launcher'ı tanımlıyoruz
+        speechResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        ArrayList<String> matches = result.getData()
+                                .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                        if (matches != null && !matches.isEmpty()) {
+                            searchView.setQuery(matches.get(0),false);
+                        }
+                    }
+                }
+        );
+    }
 
     @Nullable
     @Override
@@ -45,7 +72,7 @@ public class WordsFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         ImageButton btnAddWord = view.findViewById(R.id.btnAddWord);
-        SearchView searchView = view.findViewById(R.id.searchView);
+        searchView = view.findViewById(R.id.searchView);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -134,6 +161,24 @@ public class WordsFragment extends Fragment {
             showAddEditDialog(null); // insert
         });
 
+        ImageButton btnTr = view.findViewById(R.id.btnVoiceTr);
+        ImageButton btnEn = view.findViewById(R.id.btnVoiceEn);
+
+        btnTr.setOnClickListener(v -> startSpeechToText("tr-TR"));
+        btnEn.setOnClickListener(v -> startSpeechToText("en-US"));
+
+        speechResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        ArrayList<String> matches = result.getData()
+                                .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                        if (matches != null && !matches.isEmpty()) {
+                            searchView.setQuery(matches.get(0),false);
+                        }
+                    }
+                }
+        );
         viewModel = new ViewModelProvider(this).get(WordsViewModel.class);
         WordsRepository repository = new WordsRepository(requireContext());
 
@@ -255,6 +300,19 @@ public class WordsFragment extends Fragment {
                 })
                 .setNegativeButton("İptal", null)
                 .show();
+    }
+
+    private void startSpeechToText(String langCode) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, langCode);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Dinleniyor...");
+
+        try {
+            speechResultLauncher.launch(intent);
+        } catch (Exception e) {
+            Toast.makeText(getContext(), "Hata: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
